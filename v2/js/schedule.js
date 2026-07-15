418602cd4af4f572bef291f01bec7195b46957c4
luisgon14052-bit <luisgon14052@gmail.com>
2026-07-14 23:59:25 -0700

    Update schedule.js

--- v2/js/schedule.js
+++ v2/js/schedule.js
@@ -1,1 +1,15 @@
+const next = await getNextClass();
 
+tvState.nextClass = {
+
+    class: next.classes.name,
+
+    coach: next.coaches.name,
+
+    photo: next.coaches.photo_url,
+
+    time: next.start_time
+
+};
+
+renderNextClass();
