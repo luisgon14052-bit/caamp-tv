8c8d417d471975c70ef00be9a2a4417b5a36d53e
luisgon14052-bit <luisgon14052@gmail.com>
2026-07-14 23:59:01 -0700

    Add renderNextClass function to update UI

--- v2/js/ui.js
+++ v2/js/ui.js
@@ -1,1 +1,14 @@
+function renderNextClass() {
 
+    if (!tvState.nextClass) return;
+
+    document.getElementById("class-name").textContent =
+        tvState.nextClass.class;
+
+    document.getElementById("coach-name").textContent =
+        tvState.nextClass.coach;
+
+    document.getElementById("coach-background").style.backgroundImage =
+        `url(${tvState.nextClass.photo})`;
+
+}
